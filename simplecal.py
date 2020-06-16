8af8eb1500b89ffe8a99e247a521ad21b0ad5f86
Joseph Haaga <haaga.joe@gmail.com>
2020-06-15 21:53:59 -0400

    creating an event

--- simplecal.py
+++ simplecal.py
@@ -1,29 +1,19 @@
 from dataclasses import dataclass
-import datetime
+from datetime import datetime, timedelta
 
-EIGHT_HOURS_IN_SECONDS = 60 * 60 * 8
 
 @dataclass
-class TimeBlock:
-    start: datetime.datetime
-    duration: datetime.timedelta
-
-    def end(self):
-        return self.start + self.duration
+class Event:
+    description: str
+    start_time: datetime
+    duration: timedelta
 
 class Calendar:
-    timeblocks = [TimeBlock(start=datetime.datetime.now(), duration=datetime.timedelta(seconds=EIGHT_HOURS_IN_SECONDS))]
+    events = []
 
-    def get_timeblock_during(self, time: datetime.datetime):
-        for block in self.timeblocks:
-            if time >= block.start and time < block.end:
-                return block
+    def add_event(self, event: Event):
+        self.events += [event]
 
-    def add_event(self, description, start_time: datetime.datetime, duration: datetime.timedelta):
-        existing_timeblock = self.timeblocks.index(self.get_timeblock_during(start_time))
-        # TODO divide the existing TimeBlock into three TimeBlocks
-            # What if the event intersects multiple TimeBlocks?
-        before = None
-        event_block = None
-        after = None
+    def delete_event(self, event: Event):
+        self.events = [e for e in self.events if e != event]
 
--- test.py
+++ test.py
@@ -1,10 +1,13 @@
-import simplecal
+from datetime import datetime, timedelta
 
+import simplecal
 
 def main():
     cal = simplecal.Calendar()
-    event = simplecal.Event("My first event")
-    cal.schedule(event)
+    nine_am = datetime.now().replace(hour=9, minute=0, second=0, microsecond=0)
+    an_hour = timedelta(seconds=60*60)
+    event = simplecal.Event("My first event", nine_am, an_hour)
+    cal.add_event(event)
     breakpoint()
     print(cal)
 
