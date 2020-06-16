d78f6b4683ac00d16022ea88cba828313679bba8
Joseph Haaga <haaga.joe@gmail.com>
2020-06-15 22:34:18 -0400

    calculating free time

--- simplecal.py
+++ simplecal.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from datetime import datetime, timedelta
+import itertools
 
 
 @dataclass
@@ -12,12 +13,35 @@ class Event:
     def end(self):
         return self.start + self.duration
 
+
+NINE_AM = datetime.now().replace(hour=9, minute=0, second=0, microsecond=0)
+NINE_PM = NINE_AM + timedelta(seconds=60*60*12)
+
 class Calendar:
     events = []
+
+    def __init__(self):
+        self.add_event(Event("day start", NINE_AM, timedelta(microseconds=1)))
+        self.add_event(Event("day end", NINE_PM, timedelta(microseconds=1)))
 
     def add_event(self, event: Event):
         self.events += [event]
 
     def delete_event(self, event: Event):
         self.events = [e for e in self.events if e != event]
+
+    def get_events(self):
+        return sorted(self.events, key = lambda e: e.start)
+
+    def get_free_time_blocks(self):
+        free_time_blocks = []
+        a, b = itertools.tee(self.get_events())
+        next(b, None)
+        for i, j in zip(a, b):
+            duration = j.start - i.end
+            start = i.end
+            free_time_blocks += [Event("free time", start, duration)]
+        return free_time_blocks
+
+
 
--- test.py
+++ test.py
@@ -4,12 +4,13 @@ import simplecal
 
 def main():
     cal = simplecal.Calendar()
-    nine_am = datetime.now().replace(hour=9, minute=0, second=0, microsecond=0)
+    nine_am = datetime.now().replace(hour=9, minute=15, second=0, microsecond=0)
     an_hour = timedelta(seconds=60*60)
+    event2 = simplecal.Event("My second event", nine_am + an_hour, an_hour)
+    cal.add_event(event2)
     event = simplecal.Event("My first event", nine_am, an_hour)
     cal.add_event(event)
-    breakpoint()
-    print(cal)
+    return cal
 
 if __name__ == '__main__':
-    main()
+    cal = main()
