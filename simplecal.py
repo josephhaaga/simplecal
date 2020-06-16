7d49faa79ac4f5e3d129ae5f78cce773449d84c2
Joseph Haaga <haaga.joe@gmail.com>
2020-06-16 14:15:42 -0400

    add type annotations

--- simplecal.py
+++ simplecal.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 import datetime
 import itertools
+import typing
 
 
 @dataclass
@@ -10,20 +11,22 @@ class Event:
     duration: datetime.timedelta
 
     @property
-    def end(self):
+    def end(self) -> datetime.datetime:
         return self.start + self.duration
 
-    def contains_time(self, time: datetime.time):
+    def contains_time(self, time: datetime.time) -> bool:
         t = datetime.datetime.combine(self.start, time)
         if t >= self.start and t <= self.end:
             return True
+        return False
 
 
 NINE_AM = datetime.datetime.combine(datetime.datetime.today(), datetime.time(9, 0))
 NINE_PM = datetime.datetime.combine(datetime.datetime.today(), datetime.time(21, 0))
 
+
 class Calendar:
-    events = []
+    events: typing.List[Event] = []
 
     def __init__(self):
         day_start = Event("day start", NINE_AM, datetime.timedelta(microseconds=0))
@@ -31,22 +34,23 @@ class Calendar:
         self.add_event(day_start)
         self.add_event(day_end)
 
-    def add_event(self, event: Event):
+    def add_event(self, event: Event) -> None:
         self.events += [event]
 
-    def delete_event(self, event: Event):
+    def delete_event(self, event: Event) -> None:
         self.events = [e for e in self.events if e != event]
 
-    def get_event_at(self, time: datetime.time):
+    def get_event_at(self, time: datetime.time) -> typing.Union[Event, bool]:
         dt = datetime.datetime.combine(datetime.datetime.today(), time)
         for event in self.get_events():
             if event.contains_time(time):
                 return event
+        return False
 
-    def get_events(self):
-        return sorted(self.events, key = lambda e: e.start)
+    def get_events(self) -> typing.List:
+        return sorted(self.events, key=lambda e: e.start)
 
-    def get_free_time_blocks(self):
+    def get_free_time_blocks(self) -> typing.List:
         free_time_blocks = []
         a, b = itertools.tee(self.get_events())
         next(b, None)
@@ -56,8 +60,7 @@ class Calendar:
             free_time_blocks += [Event("free time", start, duration)]
         return free_time_blocks
 
-    def calculate_minutes_of_free_time(self):
+    def calculate_minutes_of_free_time(self) -> int:
         free_time_blocks = self.get_free_time_blocks()
         durations = [x.duration.total_seconds() for x in free_time_blocks]
         return sum(durations) // 60
-
--- test.py
+++ test.py
@@ -2,20 +2,21 @@ from datetime import datetime, timedelta, time
 
 import simplecal
 
+
 def main():
     cal = simplecal.Calendar()
     nine_am = datetime.now().replace(hour=9, minute=15, second=0, microsecond=0)
-    an_hour = timedelta(seconds=60*60)
+    an_hour = timedelta(seconds=60 * 60)
     event2 = simplecal.Event("My second event", nine_am + an_hour, an_hour)
     cal.add_event(event2)
     event = simplecal.Event("My first event", nine_am, an_hour)
     cal.add_event(event)
     return cal
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     cal = main()
     free_time = cal.get_free_time_blocks()
     print(f"At 9:16AM, I have the following event")
     first_meeting = cal.get_event_at(time(9, 16))
     print(first_meeting)
-
